221326b34745358472a55dfa4a6c05de3cae547c
Fred7877 <darkfred78@gmail.com>
2021-07-22 12:37:39 +0200

    add command recover db from file mock_data.json

---
+++ database/migrations/2021_07_22_101058_create_compagnies_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompagniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('compagnies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 45);
+            $table->string('sector', 45);
+            $table->integer('siren')->nullable()->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('compagnies');
+    }
+}
